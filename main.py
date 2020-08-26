0d83e1abe413229371aeab140de231d66f3dce96
waizui <lch01234@gmail.com>
2020-08-27 00:04:19 +0800

    music streaming

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 import sys
 from typing import Generic ,TypeVar
 from flask import request,session,redirect,url_for
-from flask import Flask
-
+from flask import Flask,render_template,Response
+from pathlib import Path
 
 
 
@@ -15,6 +15,8 @@ class AppController:
         'session_timeout':7200,
         'authenmethod':'name',
         'secret_key':'secretkey',
+        'resour_dir':'/mnt/share/LCH320G1/music/L1',
+        'users':[]
     }
 
     defalutconfpath='/appconfig.json'
@@ -48,8 +50,11 @@ class AppController:
         self._ReadConfig()
 
         self.webprovider= WebProvider.Get()
+        self.webprovider.config['SECRET_KEY']=controller._config['secret_key']
+        self.webprovider.config['PERMANENT_SESSION_LIFETIME']=controller._config['session_timeout']
 
-        self.dataprovider=DataProvider.Get()
+
+        self.dataprovider=DataProvider.Get(self)
 
         self._authcator=AuthenProvider.GetAuthencator(self)
 
@@ -72,11 +77,16 @@ class AuthcatorBase:
 class FlaskAuthcator(AuthcatorBase):
 
     def __init__(self,controller,param=None):
+        self.controller=controller
+
+        self.media_type_dic={
+            '.mp3':'audio/mp3',
+            '.flac':'audio/flac',
+
+        }
+
         #TODO:input check
         app=controller.webprovider
-        app.config['SECRET_KEY']=controller._config['secret_key']
-        app.config['PERMANENT_SESSION_LIFETIME']=controller._config['session_timeout']
-
         @app.route('/')
         def VerifyUser():
             return '<p>输入账号 </p>'\
@@ -97,12 +107,44 @@ class FlaskAuthcator(AuthcatorBase):
             session['username']=uname
             return redirect(url_for('FileBrowser'))
 
+        @app.route('/welcome',methods=['Get','Post'])
+        def WelcomePage():
+            if session.get('username') is None:
+                return  redirect(url_for('VerifyUser'))
+            return 'developing'
 
         @app.route('/filelist',methods=['Get','Post'])
         def FileBrowser():
             if session.get('username') is None:
                 return  redirect(url_for('VerifyUser'))
-            return 'test'
+            locfiles=os.listdir(controller._config['resour_dir'])
+            return render_template('file.html',files =locfiles)
+
+
+        @app.route('/filelist/<filename>',methods=['Get','Post'])
+        def GetFile(filename):
+            if session.get('username') is None:
+                return  redirect(url_for('VerifyUser'))
+            def generate(fname):
+                with open(fname,'rb') as stream:
+                    data=stream.read(1024*64)
+                    while stream:
+                        yield data
+                        data=stream.read(1024*64)
+
+
+            fpath=os.path.join(self.controller._config['resour_dir'],filename)
+            print(fpath)
+            extension=Path(fpath).suffix
+
+            if extension in self.media_type_dic:
+                mime=self.media_type_dic[extension]
+                return Response(generate(fpath),mimetype=mime)
+
+            return '<p>不支持的格式<p>'
+
+
+
 
 class WebProvider:
     _Instance=None
@@ -117,15 +159,14 @@ class WebProvider:
 class DataProvider:
     _Instance=None
     @staticmethod
-    def Get():
+    def Get(controller):
         if DataProvider._Instance is None:
-            DataProvider._Instance=DataProvider()
+            DataProvider._Instance=DataProvider(controller)
 
         return DataProvider._Instance
 
-    def __init__(self):
-        self._users = ['liuchanghe','wanglonglong']
-
+    def __init__(self,controller):
+        self._users =controller._config['users']
         self._data = {'Users':lambda: self._GetUsers()}
 
 
